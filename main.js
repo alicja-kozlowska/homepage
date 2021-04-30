bb09d9059640f32e72d69e80a6803fc887580270
Alicja Kozłowska <alikozlowska@gmail.com>
2021-04-30 23:11:22 +0200

    ordering css

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ alert('siema!');
 const firstname = 'Alicja';
 const age = '35';
 console.log(`Nazywam się ${firstname} i mam ${age} lat. Pozdrawiam :)`);
+const heading = document.querySelector ('.navigation');
+console.log(heading);
